9540a0a3a7534243b427e99d01111b96e3b7bd0f
seymurkafkas <seymurkafkas@gmail.com>
2022-06-02 23:57:07 +0300

    added a dynamics simulator, updated motor parameter data types

--- src/DriveSystems.jl
+++ src/DriveSystems.jl
@@ -1,50 +1,47 @@
-#The following piece of code is adapted from Electric Machinery Fundamentals
-#by Stephen Chapman
 # Author: Seymur Kafkas 040170911
 
 module DriveSystems
 
 struct ACSupply
-    v_phase::Float64
-    f::Float64
+    v_phase::Number
+    f::Number
 end
 
-
 struct VFControllerSettings
-    v_rated::Float64
-    f_base::Float64
+    v_rated::Number
+    f_base::Number
 end
 
 struct InductionMotorParams
-    rs::Float64 # Resistance Input
-    p_pair::Int # Pole Pairs
-    f_base::Float64    # Frequency Base for reactance values ( assume a linear relationship (X= jwl))
-    r1::Float64 # Stator Resistance
-    x1_base::Float64 # Stator Base Reactance
-    r2::Float64 # Rotor Referred Resistance (at standstill)
-    x2_base::Float64 # Rotor Base Referred Reactance
-    rfe::Float64 #Core loss Resistance
-    xm_base::Float64 # Magnetization Branch Base Reactance
-    n_n::Float64 # Nominal Speed (RPM)
+    rs::Number # Resistance Input
+    p_pair::Integer # Pole Pairs
+    f_base::Number    # Frequency Base for reactance values ( assume a linear relationship (X= jwl))
+    r1::Number # Stator Resistance
+    x1_base::Number # Stator Base Reactance
+    r2::Number # Rotor Referred Resistance (at standstill)
+    x2_base::Number # Rotor Base Referred Reactance
+    rfe::Number #Core loss Resistance
+    xm_base::Number # Magnetization Branch Base Reactance
+    n_n::Number # Nominal Speed (RPM)
 end
 
 mutable struct InductionMotorWithSupply
-    rs::Float64 # Resistance Input
-    p_pair::Int # Pole Pairs
-    f_base::Float64    # Frequency Base for reactance values ( assume a linear relationship (X= jwl))
-    r1::Float64 # Stator Resistance
-    x1_base::Float64 # Stator Base Reactance
-    x1::Float64 # Stator Reactance
-    r2::Float64 # Rotor Referred Resistance (at standstill)
-    x2_base::Float64 # Rotor Base Referred Reactance
-    x2::Float64 #Rotor Referred  Reactance
-    rfe::Float64 #Core loss Resistance
-    xm_base::Float64 # Magnetization Branch Base Reactance
-    xm::Float64 #  Magnetization Branch Reactance
-    n_n::Float64 # Nominal Speed (RPM)
-    n_sync::Float64 #Synchronous Speed
-    v_phase::Float64 # Phase Voltage    
-    f::Float64 #Frequency
+    rs::Number # Resistance Input
+    p_pair::Integer # Pole Pairs
+    f_base::Number    # Frequency Base for reactance values ( assume a linear relationship (X= jwl))
+    r1::Number # Stator Resistance
+    x1_base::Number # Stator Base Reactance
+    x1::Number # Stator Reactance
+    r2::Number # Rotor Referred Resistance (at standstill)
+    x2_base::Number # Rotor Base Referred Reactance
+    x2::Number #Rotor Referred  Reactance
+    rfe::Number #Core loss Resistance
+    xm_base::Number # Magnetization Branch Base Reactance
+    xm::Number #  Magnetization Branch Reactance
+    n_n::Number # Nominal Speed (RPM)
+    n_sync::Number #Synchronous Speed
+    v_phase::Number # Phase Voltage    
+    f::Number #Frequency
 end
 
 function make_motor_with_supply(params::InductionMotorParams, supply::ACSupply)
@@ -81,7 +78,7 @@ function make_motor_with_supply(params::InductionMotorParams, supply::ACSupply)
 end
 
 
-function update_motor!(motor::InductionMotorWithSupply, new_f::Float64, new_v_phase::Float64)
+function update_motor!(motor::InductionMotorWithSupply, new_f::Number, new_v_phase::Number)
 
     motor.n_sync = 60 * new_f / motor.p_pair
     reactance_scaling_factor = new_f / motor.f_base
@@ -101,7 +98,7 @@ function make_torque_for_slip_function(motor::InductionMotorWithSupply)
     r_th = real(z_th)
     x_th = imag(z_th)
 
-    function torque_for_slip(s::Float64)
+    function torque_for_slip(s::Number)
         pAirGap = (3 * v_th^2 * (motor.r2 + motor.rs) / s) / (((r_th + (motor.r2 + motor.rs) / s)^2 + (x_th + motor.x2)^2))
         t_ind = pAirGap / w_sync
         return t_ind
@@ -130,7 +127,7 @@ function calculate_rated_torque(motor::InductionMotorWithSupply)
 end
 
 
-function make_torque_for_speed_function(motor::InductionMotorWithSupply )
+function make_torque_for_speed_function(motor::InductionMotorWithSupply)
     torque_for_slip_function = make_torque_for_slip_function(motor)
 
     function torque_for_speed(n_m)
@@ -143,7 +140,7 @@ function make_torque_for_speed_function(motor::InductionMotorWithSupply )
 end
 
 
-function make_torque_for_speed_function_angular(motor::InductionMotorWithSupply )
+function make_torque_for_speed_function_angular(motor::InductionMotorWithSupply)
     torque_for_slip_function = make_torque_for_slip_function(motor)
     w_sync = 2 * motor.n_sync * pi / 60
 
@@ -185,7 +182,7 @@ using Plots
 
 function make_one_dimensional_range(lower, upper, delta)
 
-    length = Int(floor((upper - lower) / delta))
+    length::Integer = floor((upper - lower) / delta)
     result = collect(range(lower, upper, length=length))
     return result
 end
@@ -202,7 +199,7 @@ function plot_function(x_lower, x_upper, x_delta, func, append::Bool)
 end
 
 
-function calculate_v_for_f_for_controller(controller_settings::VFControllerSettings, frequency::Float64)
+function calculate_v_for_f_for_controller(controller_settings::VFControllerSettings, frequency::Number)
     if frequency <= controller_settings.f_base
         return frequency * controller_settings.v_rated / controller_settings.f_base
     else
@@ -211,14 +208,13 @@ function calculate_v_for_f_for_controller(controller_settings::VFControllerSetti
 end
 
 
-function set_motor_inputs_with_controller!(motor::InductionMotorWithSupply, controller_settings::VFControllerSettings, frequency_new::Float64)
+function set_motor_inputs_with_controller!(motor::InductionMotorWithSupply, controller_settings::VFControllerSettings, frequency_new::Number)
     v_phase_new = calculate_v_for_f_for_controller(controller_settings, frequency_new)
     update_motor!(motor, frequency_new, v_phase_new)
 end
 
 function get_pullout_torque_for_frequency_function(motor, controller)
     function pullout_torque_function(frequency)
-        frequency = convert(Float64, frequency)
         v_new = calculate_v_for_f_for_controller(controller, frequency)
         set_motor_inputs_with_controller!(motor, controller, frequency)
         pullout_torque = calculate_pullout_torque(motor)
@@ -246,7 +242,7 @@ function part_1()
 
     for f in frequency_values
         println(f)
-        set_motor_inputs_with_controller!(motor, controller, convert(Float64, f))
+        set_motor_inputs_with_controller!(motor, controller, f)
         torque_speed_function = make_torque_for_speed_function(motor)
         current_cumulative_plot = plot_function(n_lower, n_upper, delta_n, torque_speed_function, true)
         display(current_cumulative_plot)
@@ -258,10 +254,10 @@ end
 
 function get_load_square_torque_speed_function(square_coefficient, constant_coefficient)
     function torque_speed_function(speed)
-        if speed >= 0 
+        if speed >= 0
             return square_coefficient * speed^2 + constant_coefficient
-        else 
-            return - square_coefficient * speed^2 - constant_coefficient
+        else
+            return -square_coefficient * speed^2 - constant_coefficient
         end
     end
 
@@ -270,11 +266,16 @@ end
 
 function convert_rpm_function_to_angular(func)
     function angular_func(w_in_rad_per_second)
-        n_in_rpm = w_in_rad_per_second * 60 / (2* pi)
+        n_in_rpm = w_in_rad_per_second * 60 / (2 * pi)
         result = func(n_in_rpm)
         return result
     end
-return angular_func
+    return angular_func
+end
+
+function angular_speed_to_rpm(w_in_rad_per_second)
+    n_in_rpm = w_in_rad_per_second * 60 / (2 * pi)
+    return n_in_rpm
 end
 
 function part_2()
@@ -282,15 +283,15 @@ function part_2()
     n_upper = 6000
 
     default_supply_values = ACSupply(440, 50)
-    motor :: InductionMotorWithSupply = make_motor_with_supply(default_induction_motor_params, default_supply_values)
+    motor::InductionMotorWithSupply = make_motor_with_supply(default_induction_motor_params, default_supply_values)
     rated_torque = calculate_rated_torque(motor)
 
     load_torque_at_zero_speed = 5
-    data = [motor.n_n ^ 2   1 ;  0  1]
-    values = [rated_torque ; load_torque_at_zero_speed] 
+    data = [motor.n_n^2 1; 0 1]
+    values = [rated_torque; load_torque_at_zero_speed]
     load_parameters = inv(data) * values
-    load_torque_function = get_load_square_torque_speed_function(load_parameters[1] , load_parameters[2])
-    load_torque_curve = plot_function(n_lower,n_upper,1,load_torque_function,false)
+    load_torque_function = get_load_square_torque_speed_function(load_parameters[1], load_parameters[2])
+    load_torque_curve = plot_function(n_lower, n_upper, 1, load_torque_function, false)
     display(load_torque_curve)
     num = readline()
 end
@@ -307,21 +308,21 @@ function part_3()
 
     default_supply_values = ACSupply(440, 50)
     controller = VFControllerSettings(440, 50)
-    motor :: InductionMotorWithSupply = make_motor_with_supply(default_induction_motor_params, default_supply_values)
+    motor::InductionMotorWithSupply = make_motor_with_supply(default_induction_motor_params, default_supply_values)
 
     rated_torque = calculate_rated_torque(motor)
     load_torque_at_zero_speed = 5
-    data = [motor.n_n ^ 2   1 ;  0  1]
-    values = [rated_torque ; load_torque_at_zero_speed] 
+    data = [motor.n_n^2 1; 0 1]
+    values = [rated_torque; load_torque_at_zero_speed]
     load_parameters = inv(data) * values
-    load_torque_function = get_load_square_torque_speed_function(load_parameters[1] , load_parameters[2])
-    torque_curve = plot_function(n_lower,n_upper,1,load_torque_function,true)
+    load_torque_function = get_load_square_torque_speed_function(load_parameters[1], load_parameters[2])
+    torque_curve = plot_function(n_lower, n_upper, 1, load_torque_function, true)
 
     frequency_values = range(f_lower, f_higher, step=delta_f)
     current_cumulative_plot = nothing
 
     for f in frequency_values
-        set_motor_inputs_with_controller!(motor, controller, convert(Float64, f))
+        set_motor_inputs_with_controller!(motor, controller, f)
         torque_speed_function = make_torque_for_speed_function(motor)
         current_cumulative_plot = plot_function(n_lower, n_upper, delta_n, torque_speed_function, true)
     end
@@ -356,37 +357,49 @@ function part_5()
     frequency = 50
     v_supply = 440
 
-    controller = VFControllerSettings(440,50)
-    default_supply_values = ACSupply(440,50)
-    motor = make_motor_with_supply(default_induction_motor_params,default_supply_values)
+    controller = VFControllerSettings(440, 50)
+    default_supply_values = ACSupply(440, 50)
+    motor = make_motor_with_supply(default_induction_motor_params, default_supply_values)
 
-    j_eq = 1.25
-
+    #Calculate the Load Torque Parameters
     rated_torque = calculate_rated_torque(motor)
     load_torque_at_zero_speed = 5
-    data = [motor.n_n ^ 2   1 ;  0  1]
-    values = [rated_torque ; load_torque_at_zero_speed] 
+    data = [motor.n_n^2 1; 0 1]
+    values = [rated_torque; load_torque_at_zero_speed]
     load_parameters = inv(data) * values
 
-    load_torque_function_rpm = get_load_square_torque_speed_function(load_parameters[1] , load_parameters[2])
+    load_torque_function_rpm = get_load_square_torque_speed_function(load_parameters[1], load_parameters[2])
     load_torque_function = convert_rpm_function_to_angular(load_torque_function_rpm)
     motor_torque_function = make_torque_for_speed_function_angular(motor)
 
-    function motor_load_dynamics(state_vector,parameters,time)
+    j_eq = 1.25
+
+    function motor_load_dynamics(state_vector, parameters, time)
         w = state_vector[1]
-        bs = state_vector[2]
-        dw = (1 / j_eq) * (motor_torque_function(w)-load_torque_function(w))
-        return SVector{2}(dw,bs)
+        dw = (1 / j_eq) * (motor_torque_function(w) - load_torque_function(w))
+        return SVector{2}(dw, 0)
     end
 
-    initial_state = SVector{2}(0,0) #Start at standstill (0 velocity)
+    initial_state = SVector{2}(0.0, 0.0) #Start at standstill (0 velocity), the second state is a dummy variable
+    motor_load_system = ContinuousDynamicalSystem(motor_load_dynamics, initial_state, nothing)
+    total_simulation_time_in_s = 10
+    record_state_delta_t = 0.04 # Time step for recording the state data (Not the time step used in simulation)
+    tr = trajectory(motor_load_system, total_simulation_time_in_s, save_idxs=[1], Δt=record_state_delta_t)
+    time_array = collect(range(0, total_simulation_time_in_s, length(tr.data)))
+    angular_speeds_array = tr.data[:, 1]
 
-    println(motor_load_dynamics(initial_state,nothing,nothing))
-    motor_load_system = ContinuousDynamicalSystem(motor_load_dynamics,initial_state,nothing)
+    function unroll(x)
+        return x[1]
+    end
 
+    angular_speeds_array = map(unroll, angular_speeds_array)
+    rpm_speeds_array = map(angular_speed_to_rpm, angular_speeds_array)
+
+    res = plot(time_array, rpm_speeds_array)
+
+    display(res)
+    input = readline()
 end
-
-
 
 part_5()
 
